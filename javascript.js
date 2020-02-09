4c9f1f23e6a2cf23c0996aaf4303ea0d514ff7cc
Greg Frediani <fredianig11@gmail.com>
2020-02-09 15:56:28 +0100

    added comparison logic

--- javascript.js
+++ javascript.js
@@ -1,12 +1,43 @@
-let board = []
+let board = [];
+let playerGuess = [];
 
 
 function init(){
     createBoard();
+    game();
 }
 
 function game(){
-    
+    for(let i = 0; i < 4; i++){
+    playerGuess.push(parseInt(window.prompt("Please enter a number 0-3")));
+    }
+    compareGuesses();
+
+    console.log(playerGuess)
+}
+
+function compareGuesses(){
+    let comparisonBoard = [...board];
+    let exactMatch = 0;
+    let partialMatch = 0;
+    let noMatch = 0
+    for(let i = 0; i < 4; i++){
+        if(board[i] == playerGuess[i]){comparisonBoard.splice(i, 1); exactMatch++}
+    }
+    for(let i = 0; i < 4; i++){
+        if(comparisonBoard.includes(playerGuess[i])){comparisonBoard.splice(comparisonBoard.indexOf(playerGuess[i]),1); partialMatch++}
+        else{noMatch++}
+    }
+    console.log('exact')
+    console.log(exactMatch);
+    console.log('partial')
+    console.log(partialMatch)
+    console.log('none')
+    console.log(noMatch)
+    console.log("comparison board")
+    console.log(comparisonBoard);
+    //go through board and look for exact matches
+    //with remaining numbers look for partial match
 }
 
 function createBoard(){
@@ -16,4 +47,5 @@ function createBoard(){
 }
 
 init();
-console.log(board)
+console.log("Normie board")
+console.log(board);
